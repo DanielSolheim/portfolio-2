508f4db379fd14b030f998de54567147dcb14912
DanielSolheim <danielsolheim1@gmail.com>
2020-04-19 21:01:44 +0200

    finsihed base layout and such

---
+++ src/components/skills/SkillBlock.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function SkillBlock({skillHeading, skill1, skill2, skill3, skill4, skill5 }){
+  return(
+     <div  className="Skillblock" >
+        <h1> {skillHeading}</h1>
+         <div className="skill skill1 ">
+            <div className="skill-circle"> </div>
+            <p> {skill1} </p>
+         </div>
+         <div className="skill skill1 ">
+            <div className="skill-circle"> </div>
+            <p> {skill2} </p>
+         </div>
+         <div className="skill skill1 ">
+            <div className="skill-circle"> </div>
+            <p> {skill3} </p>
+         </div>
+         <div className="skill skill1 ">
+            <div className="skill-circle"> </div>
+            <p> {skill4} </p>
+         </div>
+         <div className="skill skill1 ">
+            <div className="skill-circle"> </div>
+            <p> {skill5} </p>
+         </div>
+    </div>
+  );
+}
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import FrontEnd from './FrontEnd';
-import Design from './Design';
-import Other from './Other';
+import SkillBlock from './SkillBlock';
 
 export default function Skills(){
   return (
     <div className="Skills">
-       <Design/>
-       <FrontEnd  />
-       <Other />
+
+           <div className="Skills--container">
+             <div className="Skills--description">
+                <h2 className="Skills--header"> Skills Overview </h2>
+                <p> I have been studying front end development for 2 years and these are some of the skills i aquired over these 2 years</p >
+              </div>
+              <SkillBlock skillHeading="Design" skill1="InDesign" skill2="Photoshop" skill3="Illustrator" skill4="UX design" skill5="Adobe XD" />
+              <SkillBlock skillHeading="Front-End" skill1="React" skill2="Javascript" skill3="Node.js" skill4="HTML/CSS/SASS" skill5="Wordpress" />
+              <SkillBlock skillHeading="Other" skill1="Wireframe" skill2="User Personas" skill3="DevOps(familiar)" skill4="Gulp/Grunt" skill5="NPM" />
+           </div>
     </div>
 
   );
